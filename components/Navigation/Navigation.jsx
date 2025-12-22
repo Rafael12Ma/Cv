f24cf5cb35e5ab516815902844a2b200c4bfff1e
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2025-12-22 22:47:58 +0200

    Changes on rem

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -5,13 +5,13 @@ export default function Navigation() {
     <>
       <nav
         className="flex justify-center p-10 
-        xs:text-[1.9rem] 
-        sm:text-[2.1rem]
-        md-text[2.4rem]
-        lg:text-[2.8rem]
-        xl:text-[3.2rem]
-        2xl:text-[3.6rem]
-        3xl:text-[4rem]
+        xs:text-[0.6rem] 
+        sm:text-[0.8rem]
+        md-text[1.1rem]
+        lg:text-[1.4rem]
+        xl:text-[1.7rem]
+        2xl:text-[2rem]
+        3xl:text-[2.3rem]
       border-purple-700 border-2 
         font-semibold rounded-2xl gap-10"
       >

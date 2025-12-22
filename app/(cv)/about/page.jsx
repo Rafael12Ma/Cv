bfbeaa787971dfc0aafb5722069f5e4194b163d4
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2025-12-23 00:02:45 +0200

    card

--- app/(cv)/about/page.jsx
+++ app/(cv)/about/page.jsx
@@ -1,7 +1,15 @@
+import logo from "@/public/images/st.png";
+import Image from "next/image";
+
 export default function HomePage() {
   return (
     <>
-      <main>about</main>
+      <main className="flex flex-col my-20">
+        <div className="bg-purple-800 rounded-t-3xl p-10 gap-5 justify-center m-auto  flex flex-col items-center h-1vhw">
+          <h1 className="font-sans">Rafael Maroufidis</h1>
+          <Image width={100} src={logo} alt="A photo of me!" />
+        </div>
+      </main>
     </>
   );
 }
--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -10,7 +10,7 @@ export default function Navigation() {
   const path = usePathname();
   return (
     <>
-      <nav className="flex justify-center p-10 text-[clamp(0.8rem,1.5vw,2rem)] border-purple-700 border-2 font-semibold font-mono rounded-2xl gap-8">
+      <nav className="flex justify-center items-center p-10 text-[clamp(0.8rem,1.5vw,2rem)] border-purple-700 border-2 font-semibold font-mono rounded-2xl gap-8">
         <Link
           className={path === "/contact" ? "text-purple-500" : undefined}
           href="/contact"

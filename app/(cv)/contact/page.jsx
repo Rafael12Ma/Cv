ed9a127ebb4dfe18d48479fe5b09d0ad5c173716
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2025-12-23 19:42:52 +0200

    contact me

--- app/(cv)/contact/page.jsx
+++ app/(cv)/contact/page.jsx
@@ -7,11 +7,13 @@ import Link from "next/link";
 export default function ContactPage() {
   return (
     <>
-      <div className="border-purple-950 border-2 m-10">
+      <div className="border-purple-950 border-2 m-10 rounded-b-2xl">
         <div className="bg-purple-600 lg:w-150 md:w-120 sm:w-80 flex flex-col justify-center items-center my-10 rounded-t-2xl mx-auto text-center p-10">
-          <h1 className="font-mono font-semibold text-2xl">Contact me</h1>
+          <h1 className="font-mono sm:text-sm md:text-md font-semibold ">
+            Contact me
+          </h1>
         </div>
-        <div className="bg-purple-500 rounded-2xl  sm:w-80 flex flex-col mx-auto justify-center">
+        <div className="bg-purple-500 rounded-b-2xl lg:w-150 md:w-120 sm:w-80   sm:w-80 flex flex-col mx-auto justify-center">
           <div className=" flex justify-evenly p-3 text-2xl ">
             <Link href="https://www.linkedin.com/in/rafail-maroufidis-79a905298/">
               {" "}

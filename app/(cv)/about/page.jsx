19e584b6a30a2408602f89c94cb27c041145ecb8
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2025-12-23 15:10:00 +0200

    card

--- app/(cv)/about/page.jsx
+++ app/(cv)/about/page.jsx
@@ -23,7 +23,7 @@ export default function HomePage() {
           <h1 className="text-purple-700 font-semibold text-2xl">
             Certifications
           </h1>
-          <ul className="grid grid-cols-2 md:grid-cols-4 sm:grid-cols-3 gap-10  text-left">
+          <ul className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10  text-left">
             {cert.map((certif) => (
               <li
                 key={certif.title}

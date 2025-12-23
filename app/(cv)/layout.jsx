64d3141c36da208686bba3f95b7d08be1926e493
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2025-12-23 20:32:01 +0200

    copyrights

--- app/(cv)/layout.jsx
+++ app/(cv)/layout.jsx
@@ -1,10 +1,16 @@
 import Navigation from "@/components/Navigation/Navigation";
+import { FaRegCopyright } from "react-icons/fa";
 
 export default function LayoutPageCV({ children }) {
   return (
     <>
       <Navigation />
       {children}
+
+      <div className="mt-50 mb-10 flex justify-center text-sm items-center opacity-40">
+        <FaRegCopyright />
+        <p>Rafael Maroufidis</p>
+      </div>
     </>
   );
 }

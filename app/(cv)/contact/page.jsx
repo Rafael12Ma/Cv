56e575115c008b06815f13c9a4c6d9800104a1d1
Rafael12Ma <rafaelmaroufidis@yahoo.com>
2025-12-23 19:06:41 +0200

    responsivness

--- app/(cv)/contact/page.jsx
+++ app/(cv)/contact/page.jsx
@@ -8,10 +8,10 @@ export default function ContactPage() {
   return (
     <>
       <div className="border-purple-950 border-2 m-10">
-        <div className="bg-purple-500 w-100 flex flex-col justify-center items-center my-10 rounded-t-2xl mx-auto text-center p-10">
+        <div className="bg-purple-500 sm:w-80 flex flex-col justify-center items-center my-10 rounded-t-2xl mx-auto text-center p-10">
           <h1 className="font-mono font-semibold">Contact me</h1>
         </div>
-        <div className="bg-blue-400 w-100 flex flex-col mx-auto justify-center">
+        <div className="bg-blue-400 sm:w-80 flex flex-col mx-auto justify-center">
           <div className=" flex justify-evenly p-3 ">
             <Link href="/a">
               {" "}
